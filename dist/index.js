c2fd55f99e49b93031b798ffca368bf4ca71c0a9
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-09 13:14:20 +0100

    chore(): added all base files

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _offsetTransformsByBounds = require('./transforms/offsetTransformsByBounds');
+
+Object.defineProperty(exports, 'offsetTransformsByBounds', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_offsetTransformsByBounds).default;
+  }
+});
+
+var _computeTMatrixFromTransforms = require('./transforms/computeTMatrixFromTransforms');
+
+Object.defineProperty(exports, 'computeTMatrixFromTransforms', {
+  enumerable: true,
+  get: function get() {
+    return _interopRequireDefault(_computeTMatrixFromTransforms).default;
+  }
+});
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// transforms
+export {default as offsetTransformsByBounds} from './transforms/offsetTransformsByBounds'
+export {default as computeTMatrixFromTransforms} from './transforms/computeTMatrixFromTransforms'
